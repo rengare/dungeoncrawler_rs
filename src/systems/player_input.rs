7ef3fbcbb28af160e09dd6be2c0e6b305bd23401
Gracjan Gorecki <gorecki.gracjan@hotmail.com>
2021-07-27 08:00:45 +0200

    feat(combat): added healing system
    When nothing happens it heals the player

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 #[system]
 #[write_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -27,6 +29,8 @@ pub fn player_input(
 
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
 
+        let mut did_something = false;
+
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
             enemies
@@ -34,6 +38,7 @@ pub fn player_input(
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(entity, _)| {
                     hit_something = true;
+                    did_something = true;
                     commands.push((
                         (),
                         WantsToAttack {
@@ -44,6 +49,7 @@ pub fn player_input(
                 });
 
             if !hit_something {
+                did_something = true;
                 commands.push((
                     (),
                     WantsToMove {
@@ -51,6 +57,16 @@ pub fn player_input(
                         destination,
                     },
                 ));
+            }
+
+            if !did_something {
+                if let Ok(mut health) = ecs
+                    .entry_mut(player_entity)
+                    .unwrap()
+                    .get_component_mut::<Health>()
+                {
+                    health.current = i32::min(health.max, health.current + 1);
+                }
             }
         }
 
